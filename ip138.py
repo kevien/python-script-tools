f380cf3d16c75c4e9725419a7803faab024e446d
m0rk <858012456@qq.com>
2016-04-06 20:21:44 +0800

    little change

--- ip138.py
+++ ip138.py
@@ -3,23 +3,23 @@ import urllib2
 import re
 
 try:
-    while True:
+  while True:
     ipaddr = raw_input("Enter IP Or Domain Name:")
     if ipaddr == "" or ipaddr == 'exit':
-        break
+      break
     else:
-        url = "http://www.ip138.com/ips138.asp?ip=%s&action=2" % ipaddr
-            u = urllib2.urlopen(url)
-                s = u.read()
-                    #Get IP Address
-                    ip = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',s)
-                        print "\n****** Below Result From IP138 Database *****"
-                            print "IP Address:",ip[0]
-                                #Get IP Address Location
-                                result = re.findall(r'(<li>.*?<>)',s)
-                                    for i in result:
-                                        print i[4:-5]
-                                            print "*"*45
-                                                print "\n"
+      url = "http://www.ip138.com/ips138.asp?ip=%s&action=2" % ipaddr
+      u = urllib2.urlopen(url)
+      s = u.read()
+      #Get IP Address
+      ip = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',s)
+      print "\n****** Below Result From IP138 Database *****"
+      print "IP Address:",ip[0]
+      #Get IP Address Location
+      result = re.findall(r'(<li>.*?</li>)',s)
+      for i in result:
+        print i[4:-5]
+      print "*"*45
+      print "\n"
 except:
-    print "No Data Found"
+  print "No Data Found"
